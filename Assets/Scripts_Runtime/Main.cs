771911ee81510f5cd8241df0ec73bc5ffc2d7e17
Jackutea <chenwansal1@163.com>
2024-04-26 21:10:00 +0800

    ESTABLISH

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FSMTutorial {
+
+    public class Main : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+            Debug.Log("Hellko");
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
